83018e1e12012e8fbffc4827ef2f5b9802cdce42
sharda maurya <shardamaurya42570@gmail.com>
2025-02-12 18:37:47 +0530

    AIquiz app created

--- account/views.py
+++ account/views.py
@@ -1,17 +1,16 @@
 from django.shortcuts import render, HttpResponse
+from django.contrib.auth.decorators import login_required
 from authapp.models import User
 from quiz.models import QuizResult
 
 # Create your views here.h
 
-
+@login_required
 def account(request):
     user = request.user
     user1 = User.objects.filter(email = user)[0]
     quizs = QuizResult.objects.filter(user=user).order_by('-submitted_at')
-    for q in quizs:
-        print(q.quiz.title)
-    
+     
     context = {
         "quizs" : quizs,
         "user" : user1,
