8f41c7bfef3f2dc402c7ad6332e3700f9688e91c
sharda maurya <shardamaurya42570@gmail.com>
2025-04-02 17:56:33 +0530

    admin and ai views issue solved

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Quiz, QuizResult, Question, UserAnswer
 
-# Register your models here.
+admin.site.register(Quiz)
+admin.site.register(QuizResult)
+admin.site.register(Question)
+admin.site.register(UserAnswer)
