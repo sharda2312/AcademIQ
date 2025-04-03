f29629e8b137b3af64fd27e700d8155393307dd3
sharda maurya <shardamaurya42570@gmail.com>
2025-04-03 18:33:43 +0530

    clever cloud mysql connected

--- AcademIQ/settings.py
+++ AcademIQ/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 import os
 
+from dotenv import load_dotenv
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -91,10 +94,10 @@ WSGI_APPLICATION = "AcademIQ.wsgi.application"
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'AcademIQ',  # Replace with your database name
-        'USER': 'user',          # Default XAMPP username
-        'PASSWORD': 'user_password',          # Leave empty if no password is set in XAMPP
-        'HOST': 'localhost',     # Use 'localhost' or '127.0.0.1'
+        'NAME': os.getenv("MYSQL_ADDON_DB"),  # Replace with your database name
+        'USER': os.getenv("MYSQL_ADDON_USER"),          # Default XAMPP username
+        'PASSWORD': os.getenv("MYSQL_ADDON_PASSWORD"),          # Leave empty if no password is set in XAMPP
+        'HOST': os.getenv("MYSQL_ADDON_HOST"),     # Use 'localhost' or '127.0.0.1'
         'PORT': '3306'
     }
 }
